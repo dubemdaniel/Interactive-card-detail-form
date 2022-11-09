b38e3fdc2d18b646de6da480216f0ce45d87d4e8
Daniel Chukwudubem <princedaniel7890@gmail.com>
2022-11-09 01:32:25 +0100

    js

---
+++ app.js
@@ -0,0 +1,60 @@
+// creating event listener for the name input
+const inputName = document.getElementById('name')
+inputName.addEventListener('keyup', () => {
+    
+    const cardName = document.querySelector('.card_name')
+
+    cardName.textContent = ''
+
+    cardName.appendChild(document.createTextNode(inputName.value))
+})
+
+// event listener for number input
+const InputNumber = document.getElementById('number')
+
+InputNumber.addEventListener('keyup', () => {
+    
+    const cardNumber = document.querySelector('.cardNum')
+
+    cardNumber.textContent = ''
+
+    cardNumber.appendChild(document.createTextNode(InputNumber.value))
+})
+
+// event listener for month date
+const monthInput = document.getElementById('date')
+
+monthInput.addEventListener('keyup', () => {
+
+    const cardMonth = document.getElementById('month');
+
+    cardMonth.textContent = ''
+
+    cardMonth.appendChild(document.createTextNode(monthInput.value))
+})
+
+// event listener for year day
+const yearInput = document.getElementById('year_date')
+
+    yearInput.addEventListener('keyup', () => {
+
+    const cardYear = document.getElementById('year');
+
+    cardYear.textContent = ''
+
+    cardYear.appendChild(document.createTextNode(yearInput.value))
+})
+
+// event listener for cvc
+const cvcInput = document.getElementById('cvc')
+
+cvcInput.addEventListener('keyup', () => {
+
+    const cardCvc = document.getElementById('_cvc')
+
+    cardCvc.textContent = ''
+
+    
+
+    cardCvc.appendChild(document.createTextNode(cvcInput.value))
+})
